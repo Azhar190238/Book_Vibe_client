c5f74d91402a2e9dc5e27866d904d48f1cbad92c
MD AZHARUDDIN <azhar73397@gmail.com>
2024-03-27 01:37:13 +0600

    alhamduliah all toast are worked properly

--- src/components/PagesToRead/PagesToRead.jsx
+++ src/components/PagesToRead/PagesToRead.jsx
@@ -2,15 +2,9 @@
 const PagesToRead = () => {
     return (
         <div>
-           <div role="tablist" className="tabs tabs-lifted">
-  <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 1" checked />
-  <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 1</div>
+           
+        <h2>i am a man</h2>
 
-  <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 2" />
-  <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 2</div>
- 
- 
-</div> 
         </div>
     );
 };
